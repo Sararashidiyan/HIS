fb0f3d3c7a34d9f5152269a9fd05309fb279aafa
sara <sararashidiyann@gmail.com>
2025-11-17 14:57:04 +0330

    personel

--- PersonelService/PersonelService.Api/Controllers/PersonelController.cs
+++ PersonelService/PersonelService.Api/Controllers/PersonelController.cs
@@ -1,1 +1,47 @@
-﻿
+﻿using Microsoft.AspNetCore.Mvc;
+
+public class PersonelController(IPersonelService _service) : ControllerBase
+{
+    [HttpGet()]
+    public async Task<IActionResult> GetAll()
+    {
+        var Personels = await _service.GetAll();
+        return Ok();
+    }
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetById([FromRoute] int id)
+    {
+        var Personel = await _service.GetById(id);
+        return Ok();
+    }
+    [HttpGet()]
+    public async Task<IActionResult> Post(AddPersonelCommand command)
+    {
+        await _service.Add(command);
+        return Ok();
+    }
+    [HttpGet()]
+    public async Task<IActionResult> Put(ModifyPersonelCommand command)
+    {
+        await _service.Modify(command);
+        return Ok();
+    }
+    [HttpPatch("activate/{id}")]
+    public async Task<IActionResult> Activate([FromRoute] int id)
+    {
+        await _service.Activate(id);
+        return Ok();
+    }
+    [HttpPatch("deactivate/{id}")]
+    public async Task<IActionResult> Deactivate([FromRoute] int id)
+    {
+        await _service.Deactivate(id);
+        return Ok();
+    }
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> Delete([FromRoute] int id)
+    {
+        await _service.Delete(id);
+        return Ok();
+    }
+}
